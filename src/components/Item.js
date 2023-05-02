9d9aaa6b651c6a39eb3f0ab26b4402c366859cd5
Historia Montague <historia.montague@outlook.com>
2023-05-02 08:16:58 -0600

    Finished Lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { useState } from "react"
 
 function Item({ name, category }) {
+  const [itemClass, updateItem] = useState("")
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={() => updateItem(!!itemClass?"":"in-cart")}>{!!itemClass?"Remove from Cart":"Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Item from "./Item";
+import { useState } from "react"
 
 function ShoppingList({ items }) {
+  const [category, updateCategory] = useState("")
+  const filter = (item) => {
+    if(item.category === category || !category) {
+      return <Item key={item.id} name={item.name} category={item.category} />
+    }
+  }
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={(e) => updateCategory(e.target.value)}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +20,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {items.map((item) => filter(item))}
       </ul>
     </div>
   );
